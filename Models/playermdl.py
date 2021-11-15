50eb5ebde4ca8160f2cc6451d2b3912e93ba5992
Tiffany CALVET <tiffany.calvet@developpement-durable.gouv.fr>
2021-11-15 17:37:56 +0100

    3ebis commit

--- Models/playermdl.py
+++ Models/playermdl.py
@@ -2,11 +2,20 @@
 
 class Player:
     def __init__(self, name, first_name, date_birth, sexe, classement):
-        self.name = name
-        self.first_name = first_name
-        self.date_birth = date_birth
-        self.sexe = sexe
-        self.classement = classement
+        player = Player(name, first_name, date_birth, sexe, classement)
+        serialized_player = {
+            'name': player.name,
+            'first_name': player.first_name,
+            'date_birth': player.date_birth,
+            'sexe': player.sexe,
+            'classement': player.classement
+        }
+        self.name = serialized_player['name']
+        self.first_name = serialized_player['first_name']
+        self.date_birth = serialized_player['date_birth']
+        self.sexe = serialized_player['sexe']
+        self.classement = serialized_player['classement']
+
 
     def __str__(self):
         return f"{self.name} {self.first_name} {self.date_birth} {self.sexe} {self.classement}"
--- Models/tournamentsmdl.py
+++ Models/tournamentsmdl.py
@@ -2,11 +2,19 @@
 
 class Tournament:
     def __init__(self, name, site, start_date, end_date, number_round=4):
-        self.name = name
-        self.site = site
-        self.start_date = start_date
-        self.end_date = end_date
-        self.number_round = number_round
+        tournament = Tournament(name,site,start_date,end_date,number_round)
+        serialized_tournament = {
+            'name': tournament.name,
+            'site': tournament.site,
+            'start_date':tournament.start_date,
+            'end_date':tournament.end_date,
+            'number_round': tournament.number_round
+        }
+        self.name = serialized_tournament['name']
+        self.site = serialized_tournament['site']
+        self.start_date = serialized_tournament['start_date']
+        self.end_date = serialized_tournament['end_date']
+        self.number_round = serialized_tournament['number_round']
 
     def choose_time (self, bullet, blitz, coup_rapide):
         self.bullet = bullet
